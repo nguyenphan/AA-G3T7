a502786e76347e9bfd90a7eb804e04014c71fb24
unknown <nguyen.phan@notabasement.com>
2012-09-27 21:51:52 +0800

    - submit stock transaction codes

--- Prototype/src/java/Entity/StockTransaction.java
+++ Prototype/src/java/Entity/StockTransaction.java
@@ -9,5 +9,44 @@ package Entity;
  * @author ptlenguyen
  */
 public class StockTransaction {
+    private int transactionId;
+    private int askId;
+    private int bidId;
+    private int price;
+
+    public StockTransaction(int askId, int bidId, int price) {
+        this.askId = askId;
+        this.bidId = bidId;
+        this.price = price;
+    }
+    
+    // for DB
+    public StockTransaction(int transactionId, int askId, int bidId, int price) {
+        this.transactionId = transactionId;
+        this.askId = askId;
+        this.bidId = bidId;
+        this.price = price;
+    }
+    
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public int getAskId() {
+        return askId;
+    }
+
+    public int getBidId() {
+        return bidId;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "StockTransaction{" + "transactionId=" + transactionId + ", askId=" + askId + ", bidId=" + bidId + ", price=" + price + '}';
+    }
     
 }
